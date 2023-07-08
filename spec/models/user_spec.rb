21baf226d9b263235904e94956db9f4eb702ea32
Ricardo Ruy <ricardo.rruy@hotmail.com>
2023-07-08 04:26:55 -0300

    Install and Config Shoulda Matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it 'creates a valid user' do
-      user = FactoryBot.create(:user)
-      expect(user).to be_valid
-    end
+  it 'creates a valid user' do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
   end
-  
+
+  describe 'associations' do
+    it { should have_many(:favorite_movies) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+end
