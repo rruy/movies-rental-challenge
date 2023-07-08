63c9dadc9c0380436618682623a62f4d68140ef5
Ricardo Ruy <ricardo.rruy@hotmail.com>
2023-07-08 04:13:23 -0300

    Install and Configure gem Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    name { 'John Doe' }
-    # email { 'john@example.com' }
+    name { Faker::Name.name }
     # Additional attributes and associations can be defined here
   end
 end
